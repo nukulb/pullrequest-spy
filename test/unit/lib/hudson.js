19511de3271babc3259817a33c52bec0f1db121f
Nukul Bhasin <me@nukulb.com>
2012-05-26 19:28:10 -0400

    Added condition for CI build server

---
+++ test/unit/lib/hudson.js
@@ -0,0 +1,14 @@
+var hudson = require("./../../../lib/hudson");
+    
+describe("hudson", function () {
+
+    beforeEach(function () {
+    });
+    
+    it("get latest build", function () {
+        hudson.getLatestBuild();
+              
+    });
+   
+   
+});
