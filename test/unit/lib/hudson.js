3075d23979466e4907bebab0044d514e422a539e
Nukul Bhasin <me@nukulb.com>
2012-05-26 21:27:14 -0400

    XHR tests for hudson

--- test/unit/lib/hudson.js
+++ test/unit/lib/hudson.js
@@ -1,13 +1,25 @@
-var hudson = require("./../../../lib/hudson");
-    
+var hudson = require("./../../../lib/hudson"),
+    XML = require("xmlhttprequest");
+
 describe("hudson", function () {
 
     beforeEach(function () {
+        spyOn(XML, "XMLHttpRequest").andCallFake(function (){
+            console.log("I am here");
+        });
     });
     
     it("get latest build", function () {
-        hudson.getLatestBuild();
-              
+        var x;
+        runs(function () {
+            hudson.getLatestBuild("BB10-WebWorks-Framework", function (data){
+                x = true;
+            });
+        });
+        waitsFor(function (){
+            return x;
+        },2000);
+                  
     });
    
    
